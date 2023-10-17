a67d8428d08a788e3637e951e4fd0027e090c800
tnraro <deokji53@gmail.com>
2023-10-17 21:24:05 +0900

    base url 추가

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -3,6 +3,7 @@ import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/resume",
   markdown: {
     rehypePlugins: [
       [rehypeExternalLinks, { rel: ["noreferrer external"] }]
